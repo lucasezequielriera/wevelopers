cb0fc09fb24b5f5b412608074fb407a0ef6a0200
Lucas Riera <lucasezequielriera@hotmail.com>
2021-09-24 09:59:28 -0300

    Details in Settings

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -17,22 +17,29 @@ export default function Settings() {
                 <Breadcrumb.Item>Settings</Breadcrumb.Item>
             </Breadcrumb>
             <div style={{ padding: 24, minHeight: 200, display: 'flex', flexFlow: 'column nowrap', justifyContent: 'center', alignItems: 'center' }}>
+            {/* User Image */}
             <Avatar size={130} icon={<UserOutlined />} src={null} style={{ backgroundColor: '#cdcdcd' }} />
-            <Input style={{ width: 350, marginTop: 20 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your full name"/>
-            <Input style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your age"/>
-            <Input style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your city"/>
+            {/* Fully Name */}
+            <Input style={{ width: 350, marginTop: 20 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your full name" defaultValue="Lucas Riera" />
+            {/* Age */}
+            <Input style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your age" defaultValue="25"/>
+            {/* City */}
+            <Input style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your city" defaultValue="Retiro, Buenos Aires, Argentina"/>
+            {/* User */}
             <Input style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your username" prefix={ <UserOutlined className="site-form-item-icon" style={{ color: 'rgb(0,142,250)' }} /> }
             suffix={
                 <Tooltip title="name@email.com or username">
                     <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
                 </Tooltip>}
-            />
+            defaultValue="lucasezequielriera@hotmail.com"/>
+            {/* Password */}
             <Input.Password style={{ width: 350, marginTop: 10 }} onChange={(change) => {change.target.value.length >= 1 ? setButtonState(false) : setButtonState(true)}} placeholder="Enter your password" prefix={<KeyOutlined className="site-form-item-icon"className="site-form-item-icon" style={{ color: 'rgb(0,142,250)' }} />} iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)} suffix={
                 <Tooltip title="Your password">
                     <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
                 </Tooltip>
                 }
-            />
+                defaultValue="lucasriera2021" />
+            {/* Submit Button */}
             <Button type="primary" htmlType="submit" style={{ marginTop: 10, width: 150 }} disabled={buttonState}>
                 Confirm details
             </Button>
